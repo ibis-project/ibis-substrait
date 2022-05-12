bc75c6217c58c167c9fd7a892dca7f143a785bfd
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-05-12 07:24:43 -0400

    chore(deps): clean up protobuf build_ext

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -9,20 +9,18 @@ in
 self: super:
 {
   protobuf = super.protobuf.overridePythonAttrs (
-    old: {
-      nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [ self.pyext ];
-      propagatedNativeBuildInputs = old.propagatedNativeBuildInputs or [ ] ++ [
+    attrs: {
+      format = "setuptools";
+      setupPyBuildFlags = attrs.setupPyBuildFlags or [ ] ++ [ "--cpp_implementation" ];
+
+      nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [ self.pyext ];
+      propagatedNativeBuildInputs = attrs.propagatedNativeBuildInputs or [ ] ++ [
         pkgs.buildPackages.protobuf
       ];
-      buildInputs = old.buildInputs or [ ] ++ [ pkgs.buildPackages.protobuf ];
+      buildInputs = attrs.buildInputs or [ ] ++ [ pkgs.buildPackages.protobuf ];
       preConfigure = ''
         export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
         export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
-      '';
-
-      preBuild = ''
-        ${self.python.pythonForBuild.interpreter} setup.py build
-        ${self.python.pythonForBuild.interpreter} setup.py build_ext --cpp_implementation
       '';
 
       installFlags = "--install-option='--cpp_implementation'";
