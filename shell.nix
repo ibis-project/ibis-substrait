805709d762b01a21dfd3c6d15bda132d4f0630b4
Gil Forsyth <gil@forsyth.dev>
2022-07-27 16:13:39 -0400

    refactor(proto): run namespacing script

--- shell.nix
+++ shell.nix
@@ -36,8 +36,14 @@ let
       find "$proto_dir" -type d -exec chmod u+rwx {} +
       find "$proto_dir" -type f -exec chmod u+rw {} +
       rm -rf ./ibis_substrait/proto
+      python proto_prefix.py "$proto_dir"/tmp substrait.ibis "$proto_dir"/substrait
+      mv "$proto_dir"/substrait substrait.bak
+      mv "$proto_dir"/tmp/substrait "$proto_dir"
       buf generate
       protol --in-place --create-package --python-out "./ibis_substrait/proto" buf
+      rm -rf "$proto_dir"/tmp
+      rm -rf "$proto_dir"/substrait
+      mv substrait.bak "$proto_dir"/substrait
     '';
   };
 in
