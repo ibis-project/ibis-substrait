31d9471156e0c56cdf65d67a740c7462fe981711
Gil Forsyth <gil@forsyth.dev>
2022-08-17 12:26:03 -0400

    ci: use dyff instead of git diff for poetry.lock diffing

--- ci/release/verify.sh
+++ ci/release/verify.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell --pure --keep POETRY_PYPI_TOKEN_PYPI -p poetry -p git -i bash
+#!nix-shell -I nixpkgs=channel:nixos-unstable-small --pure --keep POETRY_PYPI_TOKEN_PYPI -p dyff git poetry yj -i bash
 # shellcheck shell=bash
 
 set -euo pipefail
@@ -7,12 +7,8 @@ set -euo pipefail
 # verify TOML is sane
 poetry check
 
-# verify that the lock file is up to date
-# we set PYTHONHASHSEED=some_constant here
-# to avoid reordering of poetry extras
-# see https://github.com/python-poetry/poetry/issues/5125
-PYTHONHASHSEED=0 poetry lock --no-update
-git diff --exit-code poetry.lock
+# verify lock file is up to date
+bash ./dev/lockfile_diff.sh
 
 # verify that we have a token available to push to pypi using set -u
 : "${POETRY_PYPI_TOKEN_PYPI}"
