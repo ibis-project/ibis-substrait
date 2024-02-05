d8a032ff68cfde90c27a44aada11946cc2dcd1c7
Gil Forsyth <gil@forsyth.dev>
2024-02-05 13:40:25 -0500

    chore(nix): update flakes

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
   outputs = { self, flake-utils, gitignore, nixpkgs, poetry2nix, ... }@inputs: {
     overlays.default = nixpkgs.lib.composeManyExtensions [
       gitignore.overlay
-      poetry2nix.overlay
+      poetry2nix.overlays.default
       (import ./nix/overlay.nix)
     ];
   } // flake-utils.lib.eachDefaultSystem (
